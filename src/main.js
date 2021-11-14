4d7566ea10ad08faa0fbda8e995a3d07f89675d9
Xavier <xavier-sun@foxmail.com>
2021-11-14 11:08:29 +0800

    Add some semicolons

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from "vue"
-import App from "./App.vue"
-import vuetify from "./plugins/vuetify"
-import router from "./router"
-import Store from "electron-store"
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import Store from "electron-store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.prototype.$store = new Store()
+Vue.prototype.$store = new Store();
 
 const app = new Vue({
   vuetify,
   router,
   render: h => h(App)
-}).$mount("#app")
+}).$mount("#app");
 
-const darkMode = Vue.prototype.$store.get("settings.darkMode")
-app.$vuetify.theme.dark = darkMode ? darkMode : false
+const darkMode = Vue.prototype.$store.get("settings.darkMode");
+app.$vuetify.theme.dark = darkMode ? darkMode : false;
